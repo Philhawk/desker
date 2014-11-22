f44f5996fe4e5e1890b08157f923bc312e5fc2d7
rrusle <rush.lee@hotmail.com>
2014-11-22 12:20:21 +1100

    adding model

--- Desker/spec/models/desk_spec.rb
+++ Desker/spec/models/desk_spec.rb
@@ -1,24 +1,3 @@
-
-# # == Schema Information
-# #
-# # Table name: desks
-# #
-# #  id                     :integer          not null, primary key
-# #  title                  :string(255)
-# #  description            :string(255)
-# #  price                  :integer
-# #  availability           :boolean
-# #  amenities              :text
-# #  desk_type              :string(255)
-# #  thumbnail              :text
-# #  created_at             :datetime
-# #  updated_at             :datetime
-# #  thumbnail_file_name    :string(255)
-# #  thumbnail_content_type :string(255)
-# #  thumbnail_file_size    :integer
-# #  thumbnail_updated_at   :datetime
-# #
-
 # == Schema Information
 #
 # Table name: desks
@@ -39,35 +18,29 @@
 #
 
 
-#  id           :integer          not null, primary key
-#  title        :string(255)
-#  description  :string(255)
-#  price        :integer
-#  availability :boolean
-#  amenities    :text
-#  desk_type    :string(255)
-#  thumbnail    :text
-#  created_at   :datetime
-#  updated_at   :datetime
-
-#  id                     :integer          not null, primary key
-#  title                  :string(255)
-#  description            :string(255)
-#  price                  :integer
-#  availability           :boolean
-#  amenities              :text
-#  desk_type              :string(255)
-#  thumbnail              :text
-#  created_at             :datetime
-#  updated_at             :datetime
-#  thumbnail_file_name    :string(255)
-#  thumbnail_content_type :string(255)
-#  thumbnail_file_size    :integer
-#  thumbnail_updated_at   :datetime
 
 
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Desk, :type => :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Desk, :type => :model do
+  describe 'Desk' do 
+    # it 'create a desk object' do
+    # desk = Desk.new(:title => 'Good loca') 
+    # end
+
+    it 'is creates a valid desk with the required attributes(title, description, price, availability, amenities, desk_type and thumbnail)' do
+      desk = Desk.new(:title =>'', :description => '', )
+    end
+
+    it 'is invalid without a title' do 
+    end 
+
+    it 'is invalid without a description' do 
+    end 
+
+    it 'is invalid without a price' do 
+    end 
+
+    it 'is invalid without '
+  # pending "add some examples to (or delete) #{__FILE__}"
+  end
