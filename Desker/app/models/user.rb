d4904157b98eef083d84d13fdd692cdb768fc3b8
Andrew Thompson <andrew@imaginedbyandrew.com>
2014-11-23 15:12:04 +1100

    Merging so can pull latest group version from GitHub

--- Desker/app/models/user.rb
+++ Desker/app/models/user.rb
@@ -2,13 +2,17 @@
 #
 # Table name: users
 #
-#  id              :integer          not null, primary key
-#  name            :string(255)
-#  email           :string(255)
-#  password_digest :string(255)
-#  role            :string(255)
-#  created_at      :datetime
-#  updated_at      :datetime
+#  id                  :integer          not null, primary key
+#  name                :string(255)
+#  email               :string(255)
+#  password_digest     :string(255)
+#  role                :string(255)
+#  created_at          :datetime
+#  updated_at          :datetime
+#  avatar_file_name    :string(255)
+#  avatar_content_type :string(255)
+#  avatar_file_size    :integer
+#  avatar_updated_at   :datetime
 #
 
 class User < ActiveRecord::Base
