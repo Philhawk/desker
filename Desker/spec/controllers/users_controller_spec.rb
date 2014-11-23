4cf67a929a56a4432c952089c78c5d5e40cc334d
rrusle <rush.lee@hotmail.com>
2014-11-24 10:56:00 +1100

    backbone setup, user and desk testing

--- Desker/spec/controllers/users_controller_spec.rb
+++ Desker/spec/controllers/users_controller_spec.rb
@@ -1,4 +1,3 @@
-
 require 'spec_helper'
 require 'rails_helper'
 
@@ -15,10 +14,10 @@ RSpec.describe UsersController do
       end 
 
       # If a Get request is made to Index, it should be sucessful.
-      # it "should load successfuly" do
-      #   expect(response).to be_success
-      #   expect(response.status).to eq(200)
-      # end
+      it "should load successfuly" do
+        expect(response).to be_success
+        expect(response.status).to eq(200)
+      end
 
       # A new-user template is rendered.
       it "should render new user template" do
@@ -29,63 +28,33 @@ RSpec.describe UsersController do
   end 
 
   describe 'POST to create' do
-      describe 'a user with valid information' do
-        before do
-          post :create, { :user => { :firstname => 'test', :email => 'test@test.com', :password => 'test', :password_confirmation => 'test' } }
-        end
-  
-        it 'should create a user' do
-          expect(User.count) == 1
-        end
-  
-        it 'should redirect to (@user)' do
-          expect(subject).to redirect_to(assigns(:user))
-        end
+    describe 'a user with valid information' do
+      before do
+        post :create, { :user => { :firstname => 'test', :email => 'test@test.com', :password => 'test', :password_confirmation => 'test' } }
+      end
+        
+      it 'should create a user' do
+        expect(User.count) == 1
       end
   
-      describe 'a user with invalid information' do
-        before do
-          post :create, { :user => { :firstname => 'test', :email => 'test@test.com', :password => 'test', :password_confirmation => '123' } }
-        end
-  
-        it 'should create a user with invalid details' do
-          expect(User.count).to eq(0)
-        end
-  
-        it 'should render :new if not sign up not successful' do 
-          expect(response).to render_template("new")
-        end
+      it 'should redirect to (@user)' do
+        expect(response).to redirect_to(assigns(:user))
       end
-  end
+    end
+  
+    describe 'a user with invalid information' do
+      before do
+        post :create, { :user => { :firstname => 'test', :email => 'test@test.com', :password => 'test', :password_confirmation => '123' } }
+      end
+
+      it 'should create a user with invalid details' do
+        expect(User.count).to eq(0)
+     end
+
+     it 'should render :new if not sign up not successful' do 
+       expect(response).to render_template("new")
+     end
+    end
+  end   
 end
-# #Rusli Start  
-#  require 'rails_helper'
 
-#  RSpec.describe UsersController, :type => :controller do
-#   describe "GET index" do 
-
-#     # If a Get request is made to Index, it should be sucessful.
-#     it "should load successfuly" do
-#       expect(response).to be_success
-#       expect(response.status).to eq(200)
-#     end
-
-#     # A new-user template is rendered.
-#     it "should render new user template" do
-#       expect(response).to render_template('new') 
-#     end
-#   end 
-
-  # describe "POST to create" do 
-
-  #   #create a new user with valid params 
-  #   describe 'a user with a valid params' do 
-  #     before do 
-  #       post : 
-  #     end 
-
-  
-
-#Rusli Stop 
-
->>>>>>> d9328e8aed2242c5c248f10ff82a0d22ade3f3c6
