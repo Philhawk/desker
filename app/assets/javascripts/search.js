79f8e4c6391fae5b769c5eac5d17800d452aca23
rrusle <rush.lee@hotmail.com>
2014-11-26 12:16:01 +1100

    pop up hamburger

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,29 +1,29 @@
-$( document ).ready(function() {
+// $( document ).ready(function() {
 
-   var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 14,
-            center: new google.maps.LatLng(locations[0].latitude, locations[0].longitude),
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-          });
+//    var map = new google.maps.Map(document.getElementById('map'), {
+//             zoom: 14,
+//             center: new google.maps.LatLng(locations[0].latitude, locations[0].longitude),
+//             mapTypeId: google.maps.MapTypeId.ROADMAP
+//           });
 
-    for (i = 0; i < locations.length; i++) {
-        var infowindow = new google.maps.InfoWindow();
+//     for (i = 0; i < locations.length; i++) {
+//         var infowindow = new google.maps.InfoWindow();
 
-        var marker, i;
-        marker = new google.maps.Marker({
-          position: new google.maps.LatLng(locations[i].latitude, locations[i].longitude),
-          map: map
-        });
+//         var marker, i;
+//         marker = new google.maps.Marker({
+//           position: new google.maps.LatLng(locations[i].latitude, locations[i].longitude),
+//           map: map
+//         });
 
-          // console.log('marker.position: ', marker.position);
+//           // console.log('marker.position: ', marker.position);
 
-          google.maps.event.addListener(marker, 'click', (function(marker, i) {
-            return function() {
-              infowindow.setContent(locations[i].address);
-              infowindow.open(map, marker);
-            }
-          })(marker, i));
+//           google.maps.event.addListener(marker, 'click', (function(marker, i) {
+//             return function() {
+//               infowindow.setContent(locations[i].address);
+//               infowindow.open(map, marker);
+//             }
+//           })(marker, i));
 
-          // console.log('locations: ', locations[i]);
-    }
-})
+//           // console.log('locations: ', locations[i]);
+//     }
+// })
